bd51c1088de59575dc9e8552d42fcdc258c7c4de
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-07-20 22:09:44 +0530

    Second commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableNativeFeedback } from "react-native";
 
 export default function App() {
-  console.log("Testing the app");
+  console.log("testing");
+  const clickText = () => {
+    console.log("The text is clicked.");
+  };
   return (
     <View style={styles.container}>
-      <Text>Hello React Native</Text>
+      <Text numberOfLines={1} onPress={clickText}>
+        Hello React Native. I need to test realy eay long text what happended
+        after I typing some kind of text like this.
+      </Text>
+      <TouchableNativeFeedback onPress={()=>console.log("Image tapped.")}>
+      <Image
+        blurRadius={10}
+        fadeDuration={2000}
+        source={{
+          width: 200,
+          height: 100,
+          uri: "https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2RlYWRsaWVzdC1zbmFrZXMuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo4Mjh9fX0=",
+        }}
+      />
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
     </View>
   );
