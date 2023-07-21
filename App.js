186724ac02f521f4e8155586f43a7491568af686
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-07-21 12:40:18 +0530

    Third commit

--- App.js
+++ App.js
@@ -1,30 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, TouchableNativeFeedback } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableNativeFeedback,
+  SafeAreaView,
+  Dimensions,
+} from "react-native";
+
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
-  console.log("testing");
-  const clickText = () => {
-    console.log("The text is clicked.");
-  };
+  const landscape = useDeviceOrientation();
+  console.log(landscape);
   return (
-    <View style={styles.container}>
-      <Text numberOfLines={1} onPress={clickText}>
-        Hello React Native. I need to test realy eay long text what happended
-        after I typing some kind of text like this.
-      </Text>
-      <TouchableNativeFeedback onPress={()=>console.log("Image tapped.")}>
-      <Image
-        blurRadius={10}
-        fadeDuration={2000}
-        source={{
-          width: 200,
-          height: 100,
-          uri: "https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2RlYWRsaWVzdC1zbmFrZXMuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo4Mjh9fX0=",
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "50%",
+          height: landscape==='landscape' ? '100%': '30%',
         }}
-      />
-      </TouchableNativeFeedback>
-      <StatusBar style="auto" />
-    </View>
+      ></View>
+    </SafeAreaView>
   );
 }
 
